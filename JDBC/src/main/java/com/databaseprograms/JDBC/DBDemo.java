fce46168c45c0cac6f48b28386f90acf60a53ca8
s8189882 <s8189882@gmail.com>
2022-10-05 17:56:13 +0530

    Add UC1 - create a payroll service database and connect program to db

--- JDBC/src/main/java/com/databaseprograms/JDBC/DBDemo.java
+++ JDBC/src/main/java/com/databaseprograms/JDBC/DBDemo.java
@@ -1,10 +1,41 @@
 package com.databaseprograms.JDBC;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
 public class DBDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSl=false";
+		String userName = "root";
+		String password = "root";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded!");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in the classpath");
+		}
+		
+		listDrivers();
+		
+		try {
+			System.out.println("Connecting to database " + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, password);
+			System.out.println("Connection is successful!!!\n" + connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = (Driver)driverList.nextElement();
+			System.out.println("\t" + driverClass.getClass().getName());
+		}	
+	}
 }
